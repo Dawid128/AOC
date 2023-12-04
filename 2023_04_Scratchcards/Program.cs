8fe0455e13443edfba253a680c1de0726ad3932f
DCz <DCz@aucotec.com>
2023-12-04 07:54:47 +0100

    AOC: 2023_04 [Part2] [SlowSolution]

--- 2023_04_Scratchcards/Program.cs
+++ 2023_04_Scratchcards/Program.cs
@@ -6,14 +6,14 @@ var stopwatch = Stopwatch.StartNew();
 
 //Part 1
 stopwatch.Start();
-var output = Part1(input); //07:10 - 07:24
+var output = Part1(input); //15
 stopwatch.Stop();
 Console.WriteLine($"Output Part1: {output}");
 Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
 
 //Part 2
 stopwatch.Start();
-output = Part2(input);
+output = Part2(input); //30
 stopwatch.Stop();
 Console.WriteLine($"Output Part2: {output}");
 Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
@@ -36,5 +36,31 @@ object Part1(string input)
 
 object Part2(string input)
 {
-    return -1;
+    var cards = input.Split(Environment.NewLine)
+                     .Select(l => l.Split(": ")[1]
+                                   .Split(" | ")
+                                   .Select(n => n.Split(" ")
+                                                 .Where(x => !string.IsNullOrEmpty(x))
+                                                 .Select(x => int.Parse(x))
+                                                 .ToArray())
+                                   .ToArray())
+                     .ToList();
+
+    int CountWin(int[][] numbers) => numbers[0].Intersect(numbers[1]).Count();
+
+    //var cache = new Dictionary<int, int>();
+    var result = 0;
+    var queueCards = new Queue<(int Index, int CountWin)>(cards.Select((x, index) => (Index: index, CountWin: CountWin(cards[index]))));
+    while (queueCards.Count > 0)
+    {
+        result++;
+        var (index, countWin) = queueCards.Dequeue();
+        if (countWin == 0)
+            continue;
+
+        for (int i = index + 1; i <= index + countWin; i++) 
+            queueCards.Enqueue((i, CountWin(cards[i])));
+    }
+
+    return result;
 }
