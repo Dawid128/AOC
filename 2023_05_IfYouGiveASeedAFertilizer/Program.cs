595a9f628b159615aaa217eecb15add7210b133d
DCz <DCz@aucotec.com>
2023-12-05 16:44:25 +0100

    AOC: 2023_05 [Part2]

--- 2023_05_IfYouGiveASeedAFertilizer/Program.cs
+++ 2023_05_IfYouGiveASeedAFertilizer/Program.cs
@@ -1,6 +1,6 @@
-﻿using AdventCodeExtension;
-using System.ComponentModel.DataAnnotations;
+﻿//[Range][SplitRangeByRanges][Long][BigNumbers][WorkWithRangeInsteadSingleNumber]
 using System.Diagnostics;
+using Range = AdventCodeExtension.Models.Range;
 
 var input = File.ReadAllText($"Resources\\Input.txt");
 var stopwatch = Stopwatch.StartNew();
@@ -14,7 +14,7 @@ Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
 
 //Part 2
 stopwatch.Start();
-output = Part2(input);
+output = Part2(input); //XX
 stopwatch.Stop();
 Console.WriteLine($"Output Part2: {output}");
 Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
@@ -31,7 +31,13 @@ object Part1(string input)
 
 object Part2(string input)
 {
-    return -1;
+    var (seeds, convertNumberMaps) = Parse(input);
+
+    var rangeSeeds = ParseToRange(seeds);
+    foreach (var convertNumberMap in convertNumberMaps)
+        rangeSeeds = ConvertNumberRange(rangeSeeds, convertNumberMap);
+
+    return rangeSeeds.Select(x=>x.Start).Min();
 }
 
 List<long> ConvertNumbers(IList<long> seeds, ConvertNumberMap convertNumberMap)
@@ -42,13 +48,53 @@ List<long> ConvertNumbers(IList<long> seeds, ConvertNumberMap convertNumberMap)
         var nextNumber = seed;
         foreach (var rcnm in convertNumberMap.RangeConvertNumberMaps)
         {
-            if (seed >= rcnm.SourceRangeStart && seed <= rcnm.SourceRangeEnd)
+            if (seed >= rcnm.SourceRange.Start && seed <= rcnm.SourceRange.End)
             {
-                nextNumber = rcnm.DestinationRangeStart + seed - rcnm.SourceRangeStart;
+                nextNumber = rcnm.DestinationRange.Start + seed - rcnm.SourceRange.Start;
                 continue;
             }
         }
         result.Add(nextNumber);
+    }
+
+    return result;
+}
+
+List<Range> ConvertNumberRange(IList<Range> rangeSeeds, ConvertNumberMap convertNumberMap)
+{
+    List<Range> result = [];
+    foreach (var rangeSeed in rangeSeeds)
+    {
+        var matchRanges = convertNumberMap.RangeConvertNumberMaps.Where(x => x.SourceRange.IsOverlap(rangeSeed)).ToList();
+
+        //If not found any match range, get range seed without change
+        if (matchRanges.Count == 0)
+        {
+            result.Add(rangeSeed);
+            continue;
+        }
+
+        //ITERATE ranges B overlapped with range A
+        var currentStart = rangeSeed.Start;
+        foreach (var (sourceRange, destinationRange) in matchRanges.Select(x => (x.SourceRange, x.DestinationRange)))
+        {
+            //IF range A exist before range B[N]: Add part of range A before range B[N] without change
+            if (currentStart < sourceRange.Start)
+            {
+                result.Add(Range.CreateRangeBetween(currentStart, sourceRange.Start));
+                currentStart = sourceRange.Start;
+            }
+
+            //IF range A exist after range B[N]: Add part of range A before end range B[N] with changes
+            //IF range A not exist after range B[N]: add range A with changes
+            var diff = destinationRange.Start - sourceRange.Start;
+            result.Add(new Range(currentStart + diff, Math.Min(rangeSeed.End, sourceRange.End) - currentStart + 1));
+            currentStart = rangeSeed.End + 1;
+        }
+
+        //IF range A exist after range C (last B[N]): Add part of range A after last range B[N] without change 
+        if (currentStart < rangeSeed.End)
+            result.Add(Range.CreateRangeBetween(currentStart, rangeSeed.End));
     }
 
     return result;
@@ -74,10 +120,10 @@ List<long> ConvertNumbers(IList<long> seeds, ConvertNumberMap convertNumberMap)
                               .Select(x => long.Parse(x))
                               .ToArray();
 
-            rangeConvertNumberMaps.Add(new RangeConvertNumberMap(numbers[0], numbers[1], numbers[2]));
+            rangeConvertNumberMaps.Add(new RangeConvertNumberMap(new Range(numbers[0], numbers[2]), new Range(numbers[1], numbers[2])));
         }
 
-        return new ConvertNumberMap(rangeConvertNumberMaps);
+        return new ConvertNumberMap(rangeConvertNumberMaps.OrderBy(x=>x.SourceRange.Start).ToList());
     }
 
     var convertNumberMaps = split.Skip(1)
@@ -88,17 +134,24 @@ List<long> ConvertNumbers(IList<long> seeds, ConvertNumberMap convertNumberMap)
     return (seeds, convertNumberMaps);
 }
 
+List<Range> ParseToRange(List<long> seeds)
+{
+    List<Range> result = [];
+    for (int i = 0; i < seeds.Count - 1; i += 2)
+        result.Add(new Range(seeds[i], seeds[i + 1]));
 
+    return result.OrderBy(x=>x.Start).ToList();
+}
 
 readonly struct ConvertNumberMap(IList<RangeConvertNumberMap> rangeConvertNumberMaps)
 {
-    public List<RangeConvertNumberMap> RangeConvertNumberMaps{ get; } = [.. rangeConvertNumberMaps];
+    public List<RangeConvertNumberMap> RangeConvertNumberMaps { get; } = [.. rangeConvertNumberMaps];
 }
 
-readonly struct RangeConvertNumberMap(long destinationRangeStart, long sourceRangeStart, long rangeLength)
+[DebuggerDisplay("{SourceRange} -> {DestinationRange}")]
+readonly struct RangeConvertNumberMap(Range destinationRange, Range sourceRange)
 {
-    public long DestinationRangeStart { get; } = destinationRangeStart;
-    public long SourceRangeStart { get; } = sourceRangeStart;
-    public long SourceRangeEnd { get; } = sourceRangeStart + rangeLength;
-    public long RangeLength { get; } = rangeLength;
+
+    public Range DestinationRange { get; } = destinationRange;
+    public Range SourceRange { get; } = sourceRange;
 }
---
+++ AdventCodeExtension/Models/Range.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+namespace AdventCodeExtension.Models
+{
+    [DebuggerDisplay("Start:{Start}, End:{End}, Length:{Length}")]
+    public readonly struct Range(long start, long length)
+    {
+        public long Start { get; } = start;
+        public long End { get; } = start + length - 1;
+        public long Length { get; } = length;
+
+        public bool IsInside(Range otherRange) => Start >= otherRange.Start && End <= otherRange.End;
+
+        public bool IsOverlap(Range otherRange) => Math.Max(Start, otherRange.Start) < Math.Min(End, otherRange.End);
+ 
+        /// <summary>
+        /// Split the current range by the input ranges
+        /// </summary>
+        /// <param name="cutRanges">Input ranges used to split the current range</param>
+        /// <returns></returns>
+        public IEnumerable<Range> SplitByRanges(IList<Range> ranges)
+        {
+            var currentStart = Start;
+            foreach (var splitRange in ranges)
+            {
+                if (currentStart < splitRange.Start)
+                {
+                    yield return CreateRangeBetween(currentStart, splitRange.Start);
+                    currentStart = splitRange.Start;
+                }
+
+                yield return CreateRangeBetween(currentStart, Math.Min(End, splitRange.End) + 1);
+                currentStart = End + 1;
+            }
+
+            if (currentStart < End)
+                yield return CreateRangeBetween(currentStart, End);
+        }
+
+        public static Range CreateRangeBetween(long start, long end) => new(start, end - start + 1);
+    }
+}
