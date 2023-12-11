91ccbf4ab83f39791de37b856e9d414078ea380b
DCz <DCz@aucotec.com>
2023-12-11 13:35:00 +0100

    2023_11 [Part2]

--- 2023_11_CosmicExpansion/Program.cs
+++ 2023_11_CosmicExpansion/Program.cs
@@ -1,4 +1,5 @@
-﻿using AdventCodeExtension;
+﻿//[ExtendArray]
+using AdventCodeExtension;
 using AdventCodeExtension.Models;
 using System.Diagnostics;
 
@@ -14,7 +15,7 @@ Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
 
 //Part 2
 stopwatch.Start();
-output = Part2(input);
+output = Part2(input, 1000000);
 stopwatch.Stop();
 Console.WriteLine($"Output Part2: {output}");
 Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
@@ -34,9 +35,27 @@ object Part1(string input)
                    .Sum();
 }
 
-object Part2(string input)
+object Part2(string input, long expansionValue)
 {
-    return -1;
+    var map = Parse(input);
+    var (emptyRows, emptyColumns) = GetEmptyRowsAndColumns(map);
+
+    PointStructLong GetPoint(int columnId, int rowId)
+    {
+        var factorRows = emptyRows.Count(x => x < rowId) * (expansionValue - 1);
+        var factorColumns = emptyColumns.Count(x => x < columnId) * (expansionValue - 1);
+        return new PointStructLong(factorColumns + columnId, factorRows + rowId);
+    }
+
+    var galaxies = map.SelectMany((x, rowId) => x.Select((Value, columnId) => (Value, Point: GetPoint(columnId, rowId))))
+                      .Where(x => x.Value is true)
+                      .Select(x => x.Point)
+                      .ToList();
+
+
+    return galaxies.Select((x, index) => (Point: x, Points: galaxies.Skip(index + 1)))
+                   .SelectMany(x => x.Points.Select(p => Math.Abs(x.Point.X - p.X) + Math.Abs(x.Point.Y - p.Y)))
+                   .Sum();
 }
 
 bool[][] Parse(string input)
@@ -68,4 +87,23 @@ bool[][] ExpansionMap(bool[][] map)
         array2D = array2D.InsertColumn(columnId, Enumerable.Range(0, array2D.GetLength(0)).Select(x => false).ToArray());
 
     return array2D.ToJaggedArray();
+}
+
+(List<int> EmptyRows, List<int> EmptyColumns) GetEmptyRowsAndColumns(bool[][] map)
+{
+    var array2D = map.To2DArray();
+
+    var emptyRows = array2D.TakeRows()
+                           .Select((Value, Index) => (Value, Index))
+                           .Where(x => !x.Value.Any(a => a == true))
+                           .Select(x => x.Index)
+                           .ToList();
+
+    var emptyColumns = array2D.TakeColumns()
+                              .Select((Value, Index) => (Value, Index))
+                              .Where(x => !x.Value.Any(a => a == true))
+                              .Select(x => x.Index)
+                              .ToList();
+
+    return (emptyRows, emptyColumns);
 }
---
+++ AdventCodeExtension/Models/PointStructLong.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace AdventCodeExtension.Models
+{
+    [DebuggerDisplay("X = {X}, Y = {Y}")]
+    public struct PointStructLong
+    {
+        public long X { get; }
+        public long Y { get; }
+
+        public PointStructLong(Point point) : this(point.X, point.Y)
+        {
+        }
+        public PointStructLong(long x, long y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
