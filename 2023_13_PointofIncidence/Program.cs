c9bf31f9a008cf3eb14543452013a7c672555b9b
DCz <DCz@aucotec.com>
2023-12-13 08:33:06 +0100

    2023_13 [Part2]

--- 2023_13_PointofIncidence/Program.cs
+++ 2023_13_PointofIncidence/Program.cs
@@ -38,7 +38,19 @@ object Part1(string input)
 
 object Part2(string input)
 {
-    return -1;
+    var maps = input.Split(Environment.NewLine + Environment.NewLine)
+                    .Select(x => x.Split(Environment.NewLine)
+                                  .Select(l => l.Select(c => c == '#')
+                                                .ToArray())
+                                  .ToArray()
+                                  .To2DArray())
+                    .ToList();
+
+    int score = 0;
+    foreach (var map in maps)
+        score += GetMirrorWithSmudgeScore(map); ;
+
+    return score;
 }
 
 int GetMirrorScore(bool[,] map)
@@ -62,6 +74,59 @@ int GetMirrorScore(bool[,] map)
             }
 
             if (mirrorLine)
+                return i;
+        }
+
+        return -1;
+    }
+
+    var rows = map.TakeRows().ToList();
+    var index = GetMirrorLineIndex(rows);
+    if (index >= 0)
+        return (index + 1) * 100;
+
+    var columns = map.TakeColumns().ToList();
+    index = GetMirrorLineIndex(columns);
+    if (index >= 0)
+        return (index + 1);
+
+    return 0;
+}
+
+int GetMirrorWithSmudgeScore(bool[,] map)
+{
+    int GetDifferencesCount<T>(T[] array1, T[] array2)
+    {
+        int differencesCount = 0;
+
+        for (int i = 0; i < array1.Length; i++)
+            if (!EqualityComparer<T>.Default.Equals(array1[i], array2[i]))
+                differencesCount++;
+
+        return differencesCount;
+    }
+
+    int GetMirrorLineIndex(List<bool[]> list)
+    {
+        for (int i = 0; i < list.Count - 1; i++)
+        {
+            var l = i;
+            var r = i + 1;
+            int differencesCount = 0;
+            var mirrorLine = true;
+            while (l >= 0 && r <= list.Count - 1)
+            {
+                differencesCount += GetDifferencesCount(list[l], list[r]);
+                if (differencesCount > 1)
+                {
+                    mirrorLine = false;
+                    break;
+                }
+                l--;
+                r++;
+            }
+
+            if (mirrorLine && differencesCount == 1)
                 return i;
         }
 
