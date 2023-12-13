d9b6ca176990f1fedab3d7dff0bb69c706e33862
DCz <DCz@aucotec.com>
2023-12-13 08:16:43 +0100

    2023_13 [Part1]

---
+++ 2023_13_PointofIncidence/Program.cs
@@ -0,0 +1,82 @@
+﻿//[Array2D][Mirror][Horizontal][Vertical]
+using AdventCodeExtension;
+using System.Diagnostics;
+
+var input = File.ReadAllText($"Resources\\Input.txt");
+var stopwatch = Stopwatch.StartNew();
+
+//Part 1
+stopwatch.Start();
+var output = Part1(input);
+stopwatch.Stop();
+Console.WriteLine($"Output Part1: {output}");
+Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
+
+//Part 2
+stopwatch.Start();
+output = Part2(input);
+stopwatch.Stop();
+Console.WriteLine($"Output Part2: {output}");
+Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
+
+object Part1(string input)
+{
+    var maps = input.Split(Environment.NewLine + Environment.NewLine)
+                    .Select(x => x.Split(Environment.NewLine)
+                                  .Select(l => l.Select(c => c == '#')
+                                                .ToArray())
+                                  .ToArray()
+                                  .To2DArray())
+                    .ToList();
+
+    int score = 0;
+    foreach (var map in maps)
+        score += GetMirrorScore(map); ;
+
+    return score;
+}
+
+object Part2(string input)
+{
+    return -1;
+}
+
+int GetMirrorScore(bool[,] map)
+{
+    int GetMirrorLineIndex(List<bool[]> list)
+    {
+        for (int i = 0; i < list.Count - 1; i++)
+        {
+            var l = i;
+            var r = i + 1;
+            var mirrorLine = true;
+            while (l >= 0 && r <= list.Count - 1)
+            {
+                if (!list[l].SequenceEqual(list[r]))
+                {
+                    mirrorLine = false;
+                    break;
+                }
+                l--;
+                r++;
+            }
+
+            if (mirrorLine)
+                return i;
+        }
+
+        return -1;
+    }
+
+    var rows = map.TakeRows().ToList();
+    var index = GetMirrorLineIndex(rows);
+    if (index >= 0)
+        return (index + 1) * 100;
+
+    var columns = map.TakeColumns().ToList();
+    index = GetMirrorLineIndex(columns);
+    if (index >= 0)
+        return (index + 1);
+
+    return 0;
+}
