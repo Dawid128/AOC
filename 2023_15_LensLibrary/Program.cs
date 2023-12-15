8fc5e17a23d4c6dba832b17557ae1d6eb9844455
DCz <DCz@aucotec.com>
2023-12-15 08:13:55 +0100

    2023_15 [Part1]

---
+++ 2023_15_LensLibrary/Program.cs
@@ -0,0 +1,31 @@
+﻿using AdventCodeExtension;
+using System.Diagnostics;
+
+var input = File.ReadAllText($"Resources\\Input.txt");
+var stopwatch = Stopwatch.StartNew();
+
+//Part 1
+stopwatch.Start();
+var output = Part1(input);
+stopwatch.Stop();
+Console.WriteLine($"Output Part1: {output}");
+Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
+
+//Part 2
+stopwatch.Start();
+output = Part2(input);
+stopwatch.Stop();
+Console.WriteLine($"Output Part2: {output}");
+Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
+
+object Part1(string input)
+{
+    return input.Split(",")
+                .Select(x => x.Select(c => (int)c).Aggregate(0, (n1, n2) => ((n1 + n2) * 17) % 256))
+                .Sum();
+}
+
+object Part2(string input)
+{
+    return -1;
+}
