7d78fa16fa7c195ca5af20dc2b6e703e64f55e2b
DCz <DCz@aucotec.com>
2023-12-19 10:08:41 +0100

    2023_18 [Part2] [ShoelaceFormula]

--- 2023_18_LavaductLagoon/Program.cs
+++ 2023_18_LavaductLagoon/Program.cs
@@ -1,6 +1,7 @@
-﻿using AdventCodeExtension;
+﻿//[RangesVsMathFormula][ShoelaceFormula][InverseOfPicksTheorem]
 using AdventCodeExtension.Models;
 using System.Diagnostics;
+using System.Globalization;
 
 var input = File.ReadAllText($"Resources\\Input.txt");
 var stopwatch = Stopwatch.StartNew();
@@ -23,103 +24,57 @@ object Part1(string input)
 {
     var instructions = input.Split(Environment.NewLine)
                             .Select(x => x.Split(" "))
-                            .Select(x => (Direction: x[0], Value: int.Parse(x[1])))
+                            .Select(x => (Direction: x[0], Value: long.Parse(x[1])))
                             .ToList();
 
-    //Create Points
-    var nextPoint = new PointStruct(0, 0);
-    List<PointStruct> points = [nextPoint];
-    foreach (var (direction, value) in instructions)
-    {
-        nextPoint = nextPoint.Move(GetMove(direction, value));
-        points.Add(nextPoint);
-    }
-
-    //Create Map
-    var minX = points.Min(p => p.X);
-    var minY = points.Min(p => p.Y);
-    var width = points.Max(p => p.X) - minX + 1;
-    var height = points.Max(p => p.Y) - minY + 1;
-    var map = new bool[height, width];
-
-    //Corigate Points
-    points = points.Select(x => x.Move(minX * -1, minY * -1)).ToList();
-
-    //Complete Map
-    for (int i = 1; i < points.Count; i++)
-    {
-        var previousPoint = points[i - 1];
-        var currentPoint = points[i];
-
-        var diffX = currentPoint.X - previousPoint.X;
-        var diffY = currentPoint.Y - previousPoint.Y;
-        var movePoint = new PointStruct(diffX != 0 ? diffX / Math.Abs(diffX) : 0, diffY != 0 ? diffY / Math.Abs(diffY) : 0);
-        while (true)
-        {
-            map[previousPoint.Y, previousPoint.X] = true;
-            if (previousPoint == currentPoint)
-                break;
-
-            previousPoint = previousPoint.Move(movePoint);
-        }
-    }
-
-    //Fill in (required is know start point) at the moment hard coded. 
-    var startPoint = new PointStruct(43, 3);
-    var queue = new Queue<PointStruct>([startPoint]);
-    var cache = new HashSet<PointStruct>([startPoint]);
-    while (queue.Count > 0)
-    {
-        var point = queue.Dequeue();
-        map[point.Y, point.X] = true;
-
-        foreach (var adjacentPoint in point.GetAdjacentPoints4(1))
-        {
-            if (cache.Contains(adjacentPoint))
-                continue;
-            cache.Add(adjacentPoint);
-
-            if (map[adjacentPoint.Y, adjacentPoint.X])
-                continue;
-
-            queue.Enqueue(adjacentPoint);
-        }
-    }
-
-    //Thread.Sleep(1000);
-    //var a = 0;
-    //Console.WriteLine();
-    //var currentPosition = Console.GetCursorPosition();
-    //for (int rowId = 0; rowId < height; rowId++)
-    //    for (int columnId = 0; columnId < width; columnId++)
-    //    {
-    //        Console.SetCursorPosition(currentPosition.Left + columnId, currentPosition.Top + rowId);
-    //        if (map[rowId, columnId] == true)
-    //        {
-    //            Console.WriteLine("X");
-    //            a++;
-    //            continue;
-    //        }
-    //        Console.WriteLine(".");
-    //    }
-
-    //Console.SetCursorPosition(currentPosition.Left, currentPosition.Top - 1);
-    //Console.ReadKey();
-
-    //return a;
-
-    //Result
-    return map.ToJaggedArray()
-              .SelectMany(x => x)
-              .Count(x => x == true);
+    return CalculateArea(instructions);
 }
 
 object Part2(string input)
 {
-    return -1;
+    (string Direction, long Value) Parse(string part)
+    {
+        part = part.Substring(2, 6);
+        var value = int.Parse(part.AsSpan(0, 5), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+        var direction = part.Last() switch
+        {
+            '0' => "R",
+            '1' => "D",
+            '2' => "L",
+            '3' => "U",
+            _ => throw new NotImplementedException(),
+        };
+
+        return (direction, value);
+    }
+
+    var instructions = input.Split(Environment.NewLine)
+                            .Select(x => x.Split(" "))
+                            .Select(x => Parse(x[2]))
+                            .ToList();
+
+    return CalculateArea(instructions);
 }
 
-(int X, int Y) GetMove(string direction, int value)
+double CalculateArea(List<(string Direction, long Value)> instructions)
+{
+    var currentPoint = new PointStructLong(0, 0);
+    long circum = 1;
+    long area = 0;
+    foreach (var (direction, value) in instructions)
+    {
+        var move = GetMove(direction, value);
+        var nextPoint = currentPoint.Move(move);
+
+        circum += value;
+        area += currentPoint.X * nextPoint.Y - currentPoint.Y * nextPoint.X;
+        currentPoint = nextPoint;
+    }
+
+    return (area / 2) + (circum / 2) + 1;
+}
+
+(long X, long Y) GetMove(string direction, long value)
 => direction switch
 {
     "R" => (value, 0),
--- AdventCodeExtension/Models/PointStructLong.cs
+++ AdventCodeExtension/Models/PointStructLong.cs
@@ -16,5 +16,21 @@ namespace AdventCodeExtension.Models
             X = x;
             Y = y;
         }
+        public readonly PointStructLong Rotate(PointStructLong center, PointRotate rotate)
+        => rotate switch
+        {
+            PointRotate.Left => Move(center.X * -1, center.Y * -1).RotateLeft().Move(center.X, center.Y),
+            PointRotate.Right => Move(center.X * -1, center.Y * -1).RotateRight().Move(center.X, center.Y),
+            _ => throw new ArgumentOutOfRangeException(nameof(rotate))
+        };
+        public readonly PointStructLong RotateLeft() => new(Y * -1, X);
+        public readonly PointStructLong RotateRight() => new(Y, X * -1);
+
+        public readonly PointStructLong Move(PointStructLong point) => Move(point.X, point.Y);
+        public readonly PointStructLong Move((long X, long Y) coordinates) => Move(coordinates.X, coordinates.Y);
+        public readonly PointStructLong Move(long x, long y) => new(X + x, Y + y);
+
+        public static bool operator ==(PointStructLong pointL, PointStructLong pointR) => ((pointL.X, pointL.Y) == (pointR.X, pointR.Y));
+        public static bool operator !=(PointStructLong pointL, PointStructLong pointR) => ((pointL.X, pointL.Y) != (pointR.X, pointR.Y));
     }
 }
