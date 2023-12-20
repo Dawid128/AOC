cad89c5460ffd6231c3469795ab0929300eaae08
DCz <DCz@aucotec.com>
2023-12-20 10:13:53 +0100

    2023_20 [Part1]

---
+++ 2023_17_ClumsyCrucible/Program.cs
@@ -0,0 +1,102 @@
+﻿using AdventCodeExtension;
+using AdventCodeExtension.Models;
+using System.Diagnostics;
+using System.Drawing;
+using System.Security.AccessControl;
+
+var input = File.ReadAllText($"Resources\\Input.txt");
+var stopwatch = Stopwatch.StartNew();
+
+//Part 1
+stopwatch.Start();
+var output = Part1(input);
+stopwatch.Stop();
+Console.WriteLine($"Output Part1: {output}");
+Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
+
+//Part 2
+stopwatch.Start();
+output = Part2(input);
+stopwatch.Stop();
+Console.WriteLine($"Output Part2: {output}");
+Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
+
+object Part1(string input)
+{
+    var map = input.Split(Environment.NewLine)
+                   .Select(x => x.Select(c => int.Parse(c.ToString()))
+                                 .ToArray())
+                   .ToArray()
+                   .To2DArray();
+
+    return GetScore(map);
+}
+
+object Part2(string input)
+{
+    return -1;
+}
+
+int GetScore(int[,] map)
+{
+    Dictionary<(PointStruct Point, int Straights), int> cache = [];
+
+    var rowsNumber = map.GetLength(0);
+    var columnsNumber = map.GetLength(1);
+    var root = new PointStruct(0, 0);
+
+    Queue<(PointStruct Point, PointStruct previousMove, int Straights, int Sum)> queue = [];
+    queue.Enqueue((root, new PointStruct(0, 0), 0, map[root.X, root.Y]));
+    cache.Add((root, 0), int.MaxValue);
+    while (queue.Count > 0)
+    {
+        var (point, previousMove, previousStraights, sum) = queue.Dequeue();
+        var nextPoints = GetPossiblePoints(rowsNumber, columnsNumber, point, previousMove, previousStraights);
+        foreach (var (nextPoint, move, straights) in nextPoints)
+        {
+            var newSum = sum + map[root.X, root.Y];
+
+            if (cache.TryGetValue((nextPoint, straights), out var cacheSum) && newSum >= cacheSum) 
+                continue;
+
+            Console.WriteLine($"New Point: ({nextPoint.X},{nextPoint.Y}) [{straights}] = {newSum}");
+            if (!cache.ContainsKey((nextPoint, straights)))
+                cache.Add((nextPoint, straights), newSum);
+            else
+                cache[(nextPoint, straights)] = newSum;
+
+            queue.Enqueue((nextPoint, move, straights, newSum));
+        }
+    }
+
+    return -1;
+}
+
+List<(PointStruct Point, PointStruct Move, int MoveOneDirectionCount)> GetPossiblePoints(int rowsNumber, int columnsNumber, PointStruct point, PointStruct previousMove, int previousMoveOneDirectionCount)
+{
+    var result = new List<(PointStruct Point, PointStruct Move, int MoveOneDirectionCount)>();
+    foreach (var (x, y) in new[] { (0, -1), (1, 0), (0, 1), (-1, 0) })
+    {
+        //Ignore the points from which it comes
+        if ((previousMove.X, previousMove.Y) == (x * -1, y * -1))
+            continue;
+
+        var continueMoveOneDirection = false;
+        if ((previousMove.X, previousMove.Y) == (x, y))
+            continueMoveOneDirection = true;
+
+        //Ignore the points if continue straight is not possible
+        if (previousMoveOneDirectionCount == 3 && continueMoveOneDirection)
+            continue;
+
+        var newPoint = new PointStruct(point.X + (x * -1), point.Y + (y * -1));
+
+        //Ignore the points, if is outside of map
+        if ((newPoint.X < 0 || newPoint.X >= columnsNumber) || (newPoint.Y < 0 || newPoint.Y >= rowsNumber))
+            continue;
+
+        result.Add((newPoint, new PointStruct(x, y), continueMoveOneDirection ? ++previousMoveOneDirectionCount : 1));
+    }
+
+    return result;
+}
---
+++ 2023_20_PulsePropagation/Program.cs
@@ -0,0 +1,111 @@
+﻿using AdventCodeExtension;
+using System.Diagnostics;
+
+var input = File.ReadAllText($"Resources\\Input.txt");
+var stopwatch = Stopwatch.StartNew();
+
+//Part 1
+stopwatch.Start();
+var output = Part1(input);
+stopwatch.Stop();
+Console.WriteLine($"Output Part1: {output}");
+Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
+
+//Part 2
+stopwatch.Start();
+output = Part2(input);
+stopwatch.Stop();
+Console.WriteLine($"Output Part2: {output}");
+Console.WriteLine($"Done in Time: {stopwatch.ElapsedMilliseconds} ms");
+
+object Part1(string input)
+{
+    var modules = input.Split(Environment.NewLine)
+                       .Select(Parse)
+                       .ToDictionary(x => x.Key, x => (x.Type, x.HighState, x.Output));
+
+    (int LowPulseCount, int HighPulseCount) counter = new(0, 0);
+    for (int i = 1; i <= 1000; i++)
+        Execute(modules, ref counter);
+
+    return counter.LowPulseCount * counter.HighPulseCount;
+}
+
+object Part2(string input)
+{
+    return -1;
+}
+
+(string Key, string Type, bool HighState, List<string> Output) Parse(string input)
+{
+    (string Key, string Type) SelectKeyAndType(string part)
+    {
+        if (part[0] == '%')
+            return ((part[1..], "%"));
+
+        if (part[0] == '&')
+            return ((part[1..], "&"));
+
+        return ((part, "S"));
+    }
+
+    var split = input.Split(" -> ");
+    var (key, type) = SelectKeyAndType(split[0]);
+
+    return (key, type, false, split[1].Split(", ").ToList());
+}
+
+void Execute(Dictionary<string, (string Type, bool HighState, List<string> OutputModules)> modules, ref (int LowPulseCount, int HighPulseCount) counter)
+{
+    counter.LowPulseCount++; //Every time, it is from button to broadcaster
+
+    Queue<(string Key, bool HighPulse)> queue = [];
+    foreach (var nextModuleKey in modules["broadcaster"].OutputModules)
+        queue.Enqueue((nextModuleKey, false));
+
+    while (queue.Count > 0)
+    {
+        Queue<(string Key, bool HighPulse)> tempQueue = [];
+        while (queue.Count > 0)
+        {
+            var (key, highPulse) = queue.Dequeue();
+            _ = highPulse ? counter.HighPulseCount++ : counter.LowPulseCount++;
+
+            if (!modules.ContainsKey(key))
+                continue;
+
+            var module = modules[key];
+
+            if (module.Type != "%" & module.Type != "&")
+                throw new Exception($"Wrong module type: {module.Type}");
+
+            if (module.Type == "%")
+            {
+                //If input is 1, do nothing
+                if (highPulse)
+                    continue;
+
+                //Else reverse current state 
+                //Send pulses to next modules
+                module.HighState = !module.HighState;
+                foreach (var nextModule in module.OutputModules)
+                    tempQueue.Enqueue((nextModule, module.HighState));
+
+                modules[key] = module; //Save changes in main list
+                continue;
+            }
+
+            module.HighState = true;
+            var previousModules = modules.Where(x => x.Value.OutputModules.Contains(key)).ToList();
+            if (previousModules.All(x => x.Value.HighState))
+                module.HighState = false;
+
+            modules[key] = module; //Save changes in main list
+
+            foreach (var nextModule in module.OutputModules)
+                tempQueue.Enqueue((nextModule, module.HighState));
+        }
+
+        queue = tempQueue;
+    }
+}
