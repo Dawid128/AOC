9bfc321acffc32bcc018984de0d617fd6914edb8
DCz <DCz@aucotec.com>
2023-12-23 08:12:23 +0100

    2023_22 [Part2]

--- 2023_22_SandSlabs/Program.cs
+++ 2023_22_SandSlabs/Program.cs
@@ -1,4 +1,5 @@
-﻿using AdventCodeExtension.Models;
+﻿//[Recursion?][GetAllChildren?][FlatList?][HelpDictionary]
+using AdventCodeExtension.Models;
 using System.Diagnostics;
 
 var input = File.ReadAllText($"Resources\\Input.txt");
@@ -31,40 +32,9 @@ object Part1(string input)
                       .OrderBy(x => x.MinZ)
                       .ToList();
 
-    //Drop all bricks to ground
-    Dictionary<Brick, (List<Brick> Parents, List<Brick> Children)> map = [];
-    foreach (var (startPoint, endPoint, minZ, maxZ) in bricks) 
-    {
-        var firstBrick = map.Select(x => x.Key)
-                            .Where(x => x.MaxZ < minZ)
-                            .OrderByDescending(x => x.MaxZ)
-                            .FirstOrDefault(x => HasCommonArea((x.StartPoint, x.EndPoint), (startPoint, endPoint)));
-
-        var children = map.Select(x=>x.Key)
-                          .Where(x => x.MaxZ == firstBrick.MaxZ && HasCommonArea((x.StartPoint, x.EndPoint), (startPoint, endPoint)))
-                          .ToList();
-
-        if (children.Count == 0)
-        {
-            map.Add(new(startPoint, endPoint, minZ - minZ, maxZ - maxZ), ([], []));
-            continue;
-        }
-
-        var diffZ = minZ - (firstBrick.MaxZ + 1);
-        Brick newBrick = new(startPoint, endPoint, minZ - diffZ, maxZ - diffZ);
-        foreach (var child in children)
-            map[child].Parents.Add(newBrick);
-
-        if (map.ContainsKey(newBrick))
-        {
-
-        }
-        map.Add(newBrick, ([], children));
-
-    }
-
     //Calculate Score
     var score = 0;
+    var map = GetMap(bricks);
     foreach (var (key, (parents, _)) in map)
     {
         if (parents.Count == 0)
@@ -80,10 +50,84 @@ object Part1(string input)
 
 object Part2(string input)
 {
-    return -1;
+    var bricks = input.Split(Environment.NewLine)
+                      .Select(x => x.Split("~")
+                                    .Select(ParseToPoint3D)
+                                    .ToArray())
+                      .Select(x => (StartPoint: new PointStruct(x[0][0], x[0][1]),
+                                    EndPoint: new PointStruct(x[1][0], x[1][1]),
+                                    MinZ: Math.Min(x[0][2], x[1][2]),
+                                    MaxZ: Math.Max(x[0][2], x[1][2])))
+                      .OrderBy(x => x.MinZ)
+                      .ToList();
+
+    //Calculate Score
+    var map = GetMap(bricks);
+    int GetScore(Brick previousBrick, List<Brick> bricks, Dictionary<Brick, (List<Brick> Parents, List<Brick> Children)> helpMap)
+    {
+        var score = 0;
+        foreach (var brick in bricks)
+        {
+            if (!helpMap.TryGetValue(brick, out var value))
+            {
+                if (!map.TryGetValue(brick, out value))
+                    throw new Exception("Not found brick");
+
+                helpMap.Add(brick, ([.. value.Parents], [.. value.Children]));
+                value = helpMap[brick];
+            }
+
+            value.Children.Remove(previousBrick);
+            if (value.Children.Count > 0)
+                continue;
+
+            score++;
+            score += GetScore(brick, value.Parents, helpMap);
+        }
+
+        return score;
+    }
+
+    var score = 0;
+    foreach (var (key, (parents, _)) in map)
+        score += GetScore(key, [.. parents], []);
+
+    return score;
 }
 
 int[] ParseToPoint3D(string input) => input.Split(",").Select(int.Parse).ToArray();
+
+Dictionary<Brick, (List<Brick> Parents, List<Brick> Children)> GetMap(IList<(PointStruct StartPoint, PointStruct EndPoint, int MinZ, int MaxZ)> bricks)
+{
+    //Drop all bricks to ground
+    Dictionary<Brick, (List<Brick> Parents, List<Brick> Children)> map = [];
+    foreach (var (startPoint, endPoint, minZ, maxZ) in bricks)
+    {
+        var firstBrick = map.Select(x => x.Key)
+                            .Where(x => x.MaxZ < minZ)
+                            .OrderByDescending(x => x.MaxZ)
+                            .FirstOrDefault(x => HasCommonArea((x.StartPoint, x.EndPoint), (startPoint, endPoint)));
+
+        var children = map.Select(x => x.Key)
+                          .Where(x => x.MaxZ == firstBrick.MaxZ && HasCommonArea((x.StartPoint, x.EndPoint), (startPoint, endPoint)))
+                          .ToList();
+
+        if (children.Count == 0)
+        {
+            map.Add(new(startPoint, endPoint, minZ - minZ, maxZ - minZ), ([], []));
+            continue;
+        }
+
+        var diffZ = minZ - (firstBrick.MaxZ + 1);
+        Brick newBrick = new(startPoint, endPoint, minZ - diffZ, maxZ - diffZ);
+        foreach (var child in children)
+            map[child].Parents.Add(newBrick);
+
+        map.Add(newBrick, ([], children));
+    }
+
+    return map;
+}
 
 bool HasCommonArea((PointStruct Start, PointStruct End) Rect1, (PointStruct Start, PointStruct End) Rect2)
 {
